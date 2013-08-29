e04e63100610fddf4e10c3290a3b6e463dd8759a
xie.shicong@gmail.com <xie.shicong@gmail.com@yourdomain.com>
2013-08-29 22:24:13 +0000

    Updated get_neighbor_angle.m to a vectorized form

--- get_neighbor_angle.m
+++ get_neighbor_angle.m
@@ -1,34 +1,31 @@
-function angles = get_neighbor_angle(cx,cy,tref)
+function angles = get_neighbor_angle(cx,cy)
 %GET_NEIGHBOR_ANGLE
-% Given the centroids of all cells, will return NxN-array of the angles between all 
+% Given the centroids of all cells, will return NxN-array of the angles between all
 % pairs of cells, for a given reference time (e.g. tref = 1, default);
 %
 % SYNOPSIS: angles = get_neighbor_angle(centroid_x,centroid_y,tref);
 %
 % xies@mit March 2012
 
-if nargin < 3, tref = 1; end
+% [~,num_cells] = size(cx);
 
-[~,num_cells] = size(cx);
+[T,N] = size(cx);
 
-angles = nan(num_cells);
-for i = 1:num_cells
-    for j = 1:num_cells
-        focal_x = cx(:,i);
-        focal_y = cy(:,i);
-        neighbor_x = cx(:,j);
-        neighbor_y = cy(:,j);
+angles = nan(T,N,N);
+for t = 1:T
+    
+    for i = 1:N
         
-        theta = atan2(...
+        focal_x = cx(t,i);
+        focal_y = cy(t,i);
+        
+        neighbor_x = cx(t,:);
+        neighbor_y = cy(t,:);
+        
+        angles(t,i,:) = atan2(...
             bsxfun(@minus,neighbor_y,focal_y) , ...
             bsxfun(@minus,neighbor_x,focal_x) );
-%         if nargin > 5
-%             theta = bsxfun(@minus,theta,orientations(:,c));
-%         end
         
-        angles(i,j) = nanmean(theta(tref,:));
     end
     
-end
-
 end
