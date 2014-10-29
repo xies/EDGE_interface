06943aa7cb9b54250ed8886b22a137ccc708811c
xies <xies@mit.edu>
2014-10-29 11:30:08 -0400

    Minor changes

--- EDGE_scripts/align_embryos.m
+++ EDGE_scripts/align_embryos.m
@@ -39,8 +39,8 @@ end
 %% Visualizing mean properties
 
 name2plot = 'myosin_intensity';
-figure, clear H
-embryoID_OI = [1:5];
+clear H
+embryoID_OI = [16];
 
 color = hsv(numel(embryoID_OI));
 
@@ -58,27 +58,29 @@ for i = 1:numel( embryoID_OI )
     embryoID = embryoID_OI(i);
     
     time = embryo_stack(embryoID).dev_time;
-%     data = embryo_stack(embryoID).(name2plot);
-%     data = data / nanmax(data(:));
+    data = embryo_stack(embryoID).(name2plot);
+%     data = bsxfun(@minus, data, nanmean(data) );
+%     data = bsxfun(@rdivide, data, nanstd(data) );
 %     time = cat(2,cells.get_embryoID(embryoID).dev_time);
 %     data = cat(2,cells.get_embryoID(embryoID).area_sm);
 %     data = (:,[cells_wt.embryoID] == i);
-        
-        switch i
-            case 1
-                data(55:60,:) = NaN;
-            case 2
-                data(60:70,:) = NaN;
-            case 4
-                data(70:75,:) = NaN;
-            case 5
-                data(125:130,:) = NaN;
-                time = time + 60;
-        end
-    
+%         
+%         switch i
+%             case 1
+%                 data(55:60,:) = NaN;
+%             case 2
+%                 data(60:70,:) = NaN;
+%             case 4
+%                 data(70:75,:) = NaN;
+%             case 5
+%                 data(125:130,:) = NaN;
+%                 time = time + 60;
+%         end
+%     
     H(i) = shadedErrorBar(time,...
         nanmean(data,2), nanstd(data,[],2), ...
-        {'color',color(i,:)},1);
+        {'color',color(i,:)},0);
+%     H(i) = plot(time, nanmean(data,2), 'color', color(i,:) );
     
     entries{i} = ['Embryo ' num2str(embryoID) ', ' num2str(input(embryoID).dt) ' sec/frame'];
     
--- EDGE_scripts/cta_clustering.m
+++ EDGE_scripts/cta_clustering.m
@@ -1,23 +1,24 @@
 %% cta_clustering Cluster cta embryo by area (constricting v. expanding)
 
-cta_areas8 = areas_sm(:,[IDs.which] == 8);
-cta_areas9 = areas_sm(:,[IDs.which] == 9);
-cta_areas10 = areas_sm(:,[IDs.which] == 10);
+cta_areas16 = areas_sm(:,[IDs.which] == 16);
+% cta_areas9 = areas_sm(:,[IDs.which] == 9);
+% cta_areas10 = areas_sm(:,[IDs.which] == 10);
 
 %%
 
-D8 = pdist( cta_areas8(:,:)' ,@nan_eucdist);
-D9 = pdist( cta_areas9(:,:)' ,@nan_eucdist);
-D10 = pdist( cta_areas10(:,:)' ,@nan_eucdist);
+D16 = pdist( cta_areas16(:,:)' ,@nan_eucdist);
+% D9 = pdist( cta_areas9(:,:)' ,@nan_eucdist);
+% D10 = pdist( cta_areas10(:,:)' ,@nan_eucdist);
 
-Z8 = linkage(D8,'complete'); Z9 = linkage(D9,'complete'); Z10 = linkage(D10,'complete');
+Z16 = linkage(D16,'complete');
+% Z9 = linkage(D9,'complete'); Z10 = linkage(D10,'complete');
 
-c8 = cluster(Z8,'maxclust',2);
-c9 = cluster(Z9,'maxclust',2);
-c10 = cluster(Z10,'maxclust',2);
+c16 = cluster(Z16,'maxclust',2);
+% c9 = cluster(Z9,'maxclust',2);
+% c10 = cluster(Z10,'maxclust',2);
 
-[X,Y] = meshgrid(dev_time(8,:) + input(8).dt*input(8).tref,1:num_cells(8));
-pcolor(X,Y, cat(2,cta_areas8(:,c8==2),cta_areas8(:,c8==1))' ), shading flat
+[X,Y] = meshgrid(dev_time(16,:) + input(16).dt*input(16).tref,1:num_cells(16));
+pcolor(X,Y, cat(2,cta_areas16(:,c16==2),cta_areas16(:,c16==1))' ), shading flat
 % figure, pcolor( cat(2,cta_areas9(:,c9==1),cta_areas9(:,c9==2))' ), shading flat
 % figure, pcolor( cat(2,cta_areas10(:,c10==1),cta_areas10(:,c10==2))' ), shading flat
 
